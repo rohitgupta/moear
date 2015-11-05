599b42a1d58049a1b590a58fe4bf33f08f17d410
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2015-11-05 16:15:59 +0800

    [chore] Development version bumped to 0.0.2-SNAPSHOT

--- src/main/java/com/vsthost/rnd/moear/CLIApplication.java
+++ src/main/java/com/vsthost/rnd/moear/CLIApplication.java
@@ -19,7 +19,7 @@ public class CLIApplication {
     /**
      * Defines the CLIApplication library version.
      */
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2-SNAPSHOT";
 
     /**
      * Prints an error message and exits.
